7b2d49545bee1bf30fc188b05e15d09c775296a5
Will Hillhouse <wmhillhouse@gmail.com>
2019-01-11 17:31:22 -0800

    loop through dict working

--- tables/views.py
+++ tables/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render
 
 from .models import Instrument
 
+from django.core import serializers
+
 
 # Create your views here.
 def instruments(request):
 
     instrument_table = Instrument.objects.all()
 
+    data = serializers.serialize("python", Instrument.objects.all())
+
     context = {
         'title': 'Instruments',
-        'instrument_table': instrument_table
+        'instrument_table': instrument_table,
+        'data': data
     }
 
     return render(request, 'tables/instruments.html', context)
