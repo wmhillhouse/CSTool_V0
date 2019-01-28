5a5c6d24a32a92f3581bf45dc62e3d3022b6146b
Will Hillhouse <wmhillhouse@gmail.com>
2019-01-28 12:40:27 +1000

    Alarms table on instrument_details page functioning.

--- tables/models.py
+++ tables/models.py
@@ -4,22 +4,33 @@ from django.db import models
 
 # Control Object
 class CtrlObject(models.Model):
+    class Meta:
+        verbose_name_plural = "Control Objects"
+
     tag = models.CharField(unique=True, max_length=100)
     description = models.CharField(max_length=200)
     # CtrlObjectType = models.ForeignKey(CtrlObjectType, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.tag
+
 
 class CtrlObjectTypes(models.Model):
+    class Meta:
+        verbose_name_plural = "Control Object Types"
+
     id = models.PositiveSmallIntegerField(primary_key=True)
     type = models.CharField(unique=True, max_length=32)
+
+    def __str__(self):
+        return self.tag
 
 
 class Instrument(models.Model):
     tag = models.CharField(unique=True, max_length=100)
     description = models.CharField(max_length=200)
+    io_allocation = models.CharField(max_length=100, default='')
     config = models.BooleanField(default=False)
-    fat = models.BooleanField(default=False)
-    sat = models.BooleanField(default=False)
     # doc_ref = models.CharField(max_length=200, default=None)
 
     def __str__(self):
@@ -33,6 +44,9 @@ class Alarm(models.Model):
     priority = models.PositiveSmallIntegerField()
     refObject = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.tag
+
 
 class AlarmTypes(models.Model):
     class Meta:
@@ -40,6 +54,9 @@ class AlarmTypes(models.Model):
 
     id = models.PositiveSmallIntegerField(primary_key=True)
     type = models.CharField(unique=True, max_length=32)
+
+    def __str__(self):
+        return self.tag
 
 
 # Create your models here.
