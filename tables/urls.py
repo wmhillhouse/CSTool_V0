222195a6b3de5db0b5ea22b6ce6cd7e7670041d5
Will Hillhouse <wmhillhouse@gmail.com>
2019-02-18 22:24:06 +1000

    Fixed header URLs

--- tables/urls.py
+++ tables/urls.py
@@ -12,11 +12,11 @@ urlpatterns = [
     url(r'^user_login/$', views.user_login, name='user_login'),
     url(r'^user_logout/$', views.user_logout, name='user_logout'),
     # List view links'
-    path('tables/<str:table_name>', views.generic_list, name='list'),
+    path('table/<str:table_name>', views.generic_list, name='list'),
     # path('documents', views.document_list, name='document-list'),
-    path('instrument', views.generic_list, name='instrument-list'),
-    path('drive', views.generic_list, name='drive-list'),
-    path('alarm', views.generic_list, name='alarm-list'),
+    # path('instrument', views.generic_list, name='instrument-list'),
+    # path('drives', views.generic_list, name='drive-list'),
+    # path('alarms', views.generic_list, name='alarm-list'),
     # Details view links
     path('lists/<str:table_name>/<str:tag>', views.generic_details, name='details'),
     path('document/<str:tag>/', views.document_edit, name='document-edit'),
